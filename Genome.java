568bca79b6861b3bcbf10794945eac6edb96058d
jakemckenzie <jake314@uw.edu>
2018-04-07 23:14:13 -0700

    added comments and second constructor

--- Genome.java
+++ Genome.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Genome {
     /**
      * The target string is my instructor's name.
@@ -8,25 +10,44 @@ public class Genome {
     /**
      * The set of characters that is allowed in this universe for evolution.
      */
-    public static List<Character> set = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-', '\'');
+    public static ArrayList<Character> set = new ArrayList<Character>(Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-', '\''));
+    /**
+     * This is the main genetic set used for evolution. We will evolve it to obtain the target.
+     * Typicaly List is chosen for this but for random access and other libraries specific to
+     * ArrayLists I chose that. Hopefully that pans out to a better time complexity.
+     */
+
+    private ArrayList<Character> geneticSet;
     /**
      * The mutation rate for the evolution in this universe. 
      */
+    private double privateMutationRate;
 
-    private List<Character> genomeSet;
-
-     private double privateMutationRate;
-
-     /**
-      * a constructor that initializes a Genome with value ‘A’ and assigns 
+    /**
+      * A constructor that initializes a Genome with value ‘A’ and assigns 
       * the internal mutation rate. 
       * @param mutationRate The mutationRate must be between zero and one.
       */
 
-      public Genome(double mutationRate){
+    public Genome(double mutationRate){
+
         privateMutationRate = mutationRate;
-        genomeSet = new Arraylist<Character>();
-        genomeSet.add('A');
-      }
+        geneticSet = new ArrayList<Character>();
+        geneticSet.add('A');
+      
+    }
+    /**
+     * A copy constructor that initializes a Genome with the same values
+     * as the input gene.
+     * @param gene This genome allows the driver to pass the gene from inside the driver.
+     */
+    public Genome(Genome gene) {
+
+        privateMutationRate = gene.privateMutationRate;
+        geneticSet = new ArrayList<Character>();
+        for (Character c : gene.geneticSet) geneticSet.add(c); 
+    }
+
+
 }
