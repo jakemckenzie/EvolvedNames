62cfb6ad3a49189b956a3ff0f2c2c93989d8ca29
jakemckenzie <jake314@uw.edu>
2018-04-07 19:29:49 -0700

    added specifications to genome.

--- Genome.java
+++ Genome.java
@@ -1,4 +1,32 @@
 public class Genome {
-    private final String target;
+    /**
+     * The target string is my instructor's name.
+     */
     
+    private static final String target = "CHRISTOPHER PAUL MARRIOTT";
+
+    /**
+     * The set of characters that is allowed in this universe for evolution.
+     */
+    public static List<Character> set = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-', '\'');
+    /**
+     * The mutation rate for the evolution in this universe. 
+     */
+
+    private List<Character> genomeSet;
+
+     private double privateMutationRate;
+
+     /**
+      * a constructor that initializes a Genome with value ‘A’ and assigns 
+      * the internal mutation rate. 
+      * @param mutationRate The mutationRate must be between zero and one.
+      */
+
+      public Genome(double mutationRate){
+        privateMutationRate = mutationRate;
+        genomeSet = new Arraylist<Character>();
+        genomeSet.add('A');
+      }
 }
